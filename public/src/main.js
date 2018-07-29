4a92a66863ec0632abc362f7c8e172f866057df2
sst8872 <sst8872@gmail.com>
2018-07-29 10:01:28 +0900

    page coded

--- public/src/main.js
+++ public/src/main.js
@@ -1,5 +1,7 @@
 window.onload = function() {
+    // Event listener for clicking hamburger menu
     document.getElementById('hamburger').addEventListener('click', function(){
+        // Swap visible / hidden hamburgers
         var classApplied = document.getElementById('nav').className;
         if(classApplied === 'visible') {
            document.getElementById('nav').className = 'hidden';
@@ -8,9 +10,18 @@ window.onload = function() {
         }
     });
 
+    // Highlight the current page for folder in the nav bar
     let pathArray = location.pathname.split('/');
     let folderName = pathArray[1];
     if(folderName === "" || folderName === "index.html") {
-
+        document.getElementById('home').className = 'currentPage';
+    } else {
+        let nav = document.getElementById('nav');
+        let links = nav.getElementsByTagName('a');
+        for(let i=0; i < links.length; i++) {
+            if(links[i].getAttribute('href').indexOf(folderName) > -1) {
+                links[i].className = 'currentPage';
+            }
+        }
     }
 };
