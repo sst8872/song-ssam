1234f1f743024478c51846e83fd7bbeff9ec72d0
sst8872 <sst8872@gmail.com>
2018-09-01 08:39:02 +0900

    words checkup modified

--- public/src/angular/controllers/list - 복사본.js
+++ public/src/angular/controllers/list - 복사본.js
@@ -1,15 +1,13 @@
 (function() {
-  angular.module('lectures').controller('lectureCtrl', ['$scope','$sce', LectureController]);
+  angular.module('lectures').controller('lectureCtrl', LectureController);
 
-  function LectureController($scope, $sce) {
+  function LectureController() {
     const vm = this;
 
     vm.data = lectureData;
     vm.activeLecture = {};
     vm.changeActiveLecture = changeActiveLecture;
     vm.search = '';
-
-    $scope.data = lectureData;
 
     function changeActiveLecture(currentLecture) {
       vm.activeLecture = currentLecture;
@@ -18,21 +16,21 @@
 
   const lectureData = [
     {
-      type: 'Green Turtle',
+      title: 'Turtle',
       image_url:
-        'http://tv.kakao.com/embed/player/cliplink/50037162?service=daum_tistory',
-      locations: 'Tropical and subtropical oceans worldwide',
+        'http://www.what-do-turtles-eat.com/wp-content/uploads/2014/10/Sea-Turtles-Habitat.jpg',
+      sub_title: '조동사 could의 세가지 용법',
       size: 'Up to 1.5m and up to 300kg',
       lifespan: 'Over 80 years',
       diet: 'Herbivore',
       description:
-        'The green turtle is a large, weighty sea turtle with a wide, smooth carapace, or shell. It inhabits tropical and subtropical coastal waters around the world and has been observed clambering onto land to sunbathe. It is named not for the color of its shell, which is normally brown or olive depending on its habitat, but for the greenish color of its skin. There are two types of green turtles—scientists are currently debating whether they are subspecies or separate species—including the Atlantic green turtle, normally found off the shores of Europe and North America, and the Eastern Pacific green turtle, which has been found in coastal waters from Alaska to Chile.'
+        'The green turtle is a large, weighty sea turtle with a wide, smooth carapace, or shell. It inhabits tropical and subtropical coastal waters around the world and has been observed clambering onto land to sunbathe. It is named not for the color of its shell, which is normally brown or olive depending on its habitat, but for the greenish color of its skin. There are two titles of green turtles—scientists are currently debating whether they are subspecies or separate species—including the Atlantic green turtle, normally found off the shores of Europe and North America, and the Eastern Pacific green turtle, which has been found in coastal waters from Alaska to Chile.'
     },
     {
-      type: 'Loggerhead Turtle',
+      title: '조동사 could',
       image_url:
         'http://i.telegraph.co.uk/multimedia/archive/02651/loggerheadTurtle_2651448b.jpg',
-      locations: 'Tropical and subtropical oceans worldwide',
+      sub_title: 'Tropical and subtropical oceans worldwide',
       size: '90cm, 115kg',
       lifespan: 'More than 50 years',
       diet: 'Carnivore',
@@ -40,10 +38,10 @@
         "Loggerhead turtles are the most abundant of all the marine turtle species in U.S. waters. But persistent population declines due to pollution, shrimp trawling, and development in their nesting areas, among other factors, have kept this wide-ranging seagoer on the threatened species list since 1978. Their enormous range encompasses all but the most frigid waters of the world's oceans. They seem to prefer coastal habitats, but often frequent inland water bodies and will travel hundreds of miles out to sea."
     },
     {
-      type: 'Leatherback Turtle',
+      title: 'Leatherback Turtle',
       image_url:
         'https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/8/13/1313246505515/Leatherback-turtle-007.jpg',
-      locations: 'All tropical and subtropical oceans',
+      sub_title: 'All tropical and subtropical oceans',
       size: 'Up to 2m, up to 900kg',
       lifespan: '45 years',
       diet: 'Carnivore',
@@ -51,10 +49,10 @@
         'Leatherbacks are the largest turtles on Earth, growing up to seven feet (two meters) long and exceeding 2,000 pounds (900 kilograms). These reptilian relics are the only remaining representatives of a family of turtles that traces its evolutionary roots back more than 100 million years. Once prevalent in every ocean except the Arctic and Antarctic, the leatherback population is rapidly declining in many parts of the world. While all other sea turtles have hard, bony shells, the inky-blue carapace of the leatherback is somewhat flexible and almost rubbery to the touch. Ridges along the carapace help give it a more hydrodynamic structure. Leatherbacks can dive to depths of 4,200 feet (1,280 meters)—deeper than any other turtle—and can stay down for up to 85 minutes.'
     },
     {
-      type: 'Hawksbill Sea Turtle',
+      title: 'Hawksbill Sea Turtle',
       image_url:
         'http://assets.worldwildlife.org/photos/163/images/carousel_small/SCR_290360hawskbill-why-matter-LG.jpg?1345565532',
-      locations: 'Tropical Coastal areas around the world',
+      sub_title: 'Tropical Coastal areas around the world',
       size: 'Over 1m, 45-68kg',
       lifespan: '30-50 Years',
       diet: 'Carnivore',
@@ -62,10 +60,10 @@
         'Dolor possimus voluptas hic aliquam rem doloremque minus maiores accusantium? Laborum perferendis harum blanditiis quod quia? Aspernatur sunt et fuga delectus ab rem excepturi. Ipsa quibusdam facere consequuntur magnam vitae? Consectetur consectetur necessitatibus beatae delectus quibusdam in! Est nobis omnis iusto illum fugiat maxime! Neque fugiat reiciendis sequi corrupti minima facere distinctio aliquam est voluptatibus. Sint incidunt soluta atque ducimus.'
     },
     {
-      type: 'Alligator Snapping Turtle',
+      title: 'Alligator Snapping Turtle',
       image_url:
         'https://upload.wikimedia.org/wikipedia/commons/e/e3/Alligator_snapping_turtle_-_Geierschildkr%C3%B6te_-_Alligatorschildkr%C3%B6te_-_Macrochelys_temminckii_01.jpg',
-      locations: 'Along the Atlantic Coast of USA',
+      sub_title: 'Along the Atlantic Coast of USA',
       size: 'around 60cm, up to 100kg',
       lifespan: '20-70 years',
       diet: 'Carnivore',
@@ -73,10 +71,10 @@
         "The prehistoric-looking alligator snapping turtle is the largest freshwater turtle in North America and among the largest in the world. With its spiked shell, beaklike jaws, and thick, scaled tail, this species is often referred to as the 'dinosaur of the turtle world.' Found almost exclusively in the rivers, canals, and lakes of the southeastern United States, alligator snappers can live to be 50 to 100 years old. Males average 26 inches (66 centimeters) in shell length and weigh about 175 pounds (80 kilograms), although they have been known to exceed 220 pounds (100 kilograms). The much smaller females top out at around 50 pounds (23 kilograms)."
     },
     {
-      type: "Kemp's Ridley Sea Turtle",
+      title: "Kemp's Ridley Sea Turtle",
       image_url:
         "https://upload.wikimedia.org/wikipedia/commons/7/7f/Kemp's_Ridley_sea_turtle_nesting.JPG",
-      locations: 'Coastal areas of the North Atlantic',
+      sub_title: 'Coastal areas of the North Atlantic',
       size: '65cm, up to 45kg',
       lifespan: 'Around 50 years',
       diet: 'Omnivore',
@@ -84,10 +82,10 @@
         'The Kemp’s ridley turtle is the world’s most endangered sea turtle, and with a worldwide female nesting population roughly estimated at just 1,000 individuals, its survival truly hangs in the balance. Their perilous situation is attributed primarily to the over-harvesting of their eggs during the last century. And though their nesting grounds are protected and many commercial fishing fleets now use turtle excluder devices in their nets, these turtles have not been able to rebound. For this reason, their nesting processions, called arribadas, make for especially high drama. During an arribada, females take over entire portions of beaches, lugging their big bodies through the sand with their flippers until they find a satisfying spot to lay their eggs. Even more riveting is the later struggle to the ocean of each tiny, vulnerable hatchling. Beset by predators, hatchlings make this journey at night, breaking out of their shells using their caruncle, a single temporary tooth grown just for this purpose.'
     },
     {
-      type: 'Olive Ridley Turtle',
+      title: 'Olive Ridley Turtle',
       image_url:
         'http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/ridley-sea-turtle_688_600x450.jpg',
-      locations: 'Tropical coastal areas around the world',
+      sub_title: 'Tropical coastal areas around the world',
       size: '70cm, 45kg',
       lifespan: '50 years',
       diet: 'Omnivore',
@@ -95,10 +93,10 @@
         'The olive ridley turtle is named for the generally greenish color of its skin and shell, or carapace. It is closely related to the Kemp’s ridley, with the primary distinction being that olive ridleys are found only in warmer waters, including the southern Atlantic, Pacific and Indian Oceans. Olive and Kemp’s ridleys are the smallest of the sea turtles, weighing up to 100 pounds (45 kilograms) and reaching only about 2 feet (65 centimeters) in shell length. The olive ridley has a slightly smaller head and smaller shell than the Kemp’s.'
     },
     {
-      type: 'Eastern Snake Necked Turtle',
+      title: 'Eastern Snake Necked Turtle',
       image_url:
         'https://c1.staticflickr.com/3/2182/2399413165_bcc8031cac_z.jpg?zz=1',
-      locations: 'Eastern Australia',
+      sub_title: 'Eastern Australia',
       size: 'Up to 30cm',
       lifespan: '25 years',
       diet: 'Carnivore',
